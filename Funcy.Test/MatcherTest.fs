bdf0641abf2bf8748da866166c5704dd60c0c2a9
Gab_km <the25thcromosome@gmail.com>
2015-06-18 06:58:23 +0900

    Add Matcher.cs and MatcherTest.fs

---
+++ Funcy.Test/MatcherTest.fs
@@ -0,0 +1,44 @@
+﻿namespace Funcy.Test
+
+open Funcy
+open Persimmon
+
+module MatcherTest =
+    let t1 = test "Matcher should perform the value pattern matching 1" {
+        let target = 10
+        let is9 = ref false
+        let is10 = ref false
+        let isElse = ref false
+        let matcher = Matcher.Match(target)
+                                .Case(9, fun () -> is9 := true)
+                                .Case(10, fun () -> is10 := true)
+                                .Else(fun () -> isElse := true)
+        do! assertPred <| not !is9
+        do! assertPred !is10
+        do! assertPred <| not !isElse
+    }
+    
+    let t2 = test "Matcher should perform the value pattern matching 2" {
+        let target = System.DateTime(2015, 6, 17)
+        let is20150617 = ref false
+        let is20150618 = ref false
+        let isElse = ref false
+        let matcher = Matcher.Match(target)
+                                .Case(System.DateTime(2015, 6, 17), fun () -> is20150617 := true)
+                                .Case(System.DateTime(2015, 6, 18), fun () -> is20150618 := true)
+                                .Else(fun () -> isElse := true)
+        do! assertPred !is20150617
+        do! assertPred <| not !is20150618
+        do! assertPred <| not !isElse
+    }
+
+    let t3 = test "Matcher should perform the value pattern matching 3" {
+        let target = "hoge"
+        let isCase = ref false
+        let isElse = ref false
+        let matcher = Matcher.Match(target)
+                                .Case("fuga", fun () -> isCase := true)
+                                .Else(fun () -> isElse := true)
+        do! assertPred <| not !isCase
+        do! assertPred !isElse
+    }
---
+++ Funcy/Matcher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcy
+{
+    public static class Matcher
+    {
+        public static Matcher<T> Match<T>(T target) where T : IEquatable<T>
+        {
+            return new Matcher<T>(target, false);
+        }
+    }
+
+    public class Matcher<T> where T : IEquatable<T>
+    {
+        private T target;
+        private bool matched;
+
+        public Matcher(T target, bool matched)
+        {
+            this.target = target;
+            this.matched = matched;
+        }
+
+        public Matcher<T> Case(T expected, Action action)
+        {
+            if (!this.matched && this.target.Equals(expected))
+            {
+                action.Invoke();
+                return new Matcher<T>(target, true);
+            }
+            else
+            {
+                return this;
+            }
+        }
+
+        public void Else(Action action)
+        {
+            if (!this.matched)
+            {
+                action.Invoke();
+            }
+        }
+    }
+}
