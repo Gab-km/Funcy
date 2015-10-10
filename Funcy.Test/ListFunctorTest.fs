7760dd19f2e7be583d3b7a0690ba9d6313fe4876
cocoatomo <cocoatomo77@gmail.com>
2015-10-10 23:04:20 +0900

    Fix test cases

--- Funcy.Test/ListFunctorTest.fs
+++ Funcy.Test/ListFunctorTest.fs
@@ -10,19 +10,19 @@ module ListFunctorTest =
     let ``fmap List<int> (int -> int) = List<int>`` = test {
         let list = List.Construct(1, 2, 3)
         let sut = list.FMap(Func<int, int>(fun x -> x * 4))
-        do! assertEquals typeof<List<int>> <| sut.GetType()
-        do! assertEquals sut <| List.Construct(4, 8, 16)
+        do! assertEquals typeof<Cons<int>> <| sut.GetType()
+        do! assertEquals sut <| List.Construct(4, 8, 12)
     }
 
     let ``fmap List<decimal> (decimal -> string) = List<string>`` = test {
         let sut = List.Construct(2.718m).FMap(Func<decimal, string>(fun d -> d.ToString()))
-        do! assertEquals typeof<List<string>> <| sut.GetType()
+        do! assertEquals typeof<Cons<string>> <| sut.GetType()
         do! assertEquals sut <| List.Construct("2.718")
     }
 
     let ``fmap Nil<byte []> (byte [] -> byte) = byte) = Nil<byte>`` = test {
         let nil = List<byte []>.Nil()
         let sut = nil.FMap(Func<byte [], byte>(fun arr -> Array.sum arr))
-        do! assertEquals typeof<None<byte>> <| sut.GetType()
+        do! assertEquals typeof<Nil<byte>> <| sut.GetType()
         do! assertEquals sut <| (List<byte>.Nil() :> List<byte>)
     }
